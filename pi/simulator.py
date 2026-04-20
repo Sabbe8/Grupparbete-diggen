2236b73893daad36e57fc04ad8e29dbbe2f0b265
Sabbe8 <sabel.calle@gmail.com>
2026-04-20 11:01:05 +0200

    Update simulator.py

--- pi/simulator.py
+++ pi/simulator.py
@@ -4,7 +4,7 @@ import argparse
 import time
 
 def getMovement(src, dst):
-    speed = 0.0002
+    speed = 0.00005
     dst_x, dst_y = dst
     x, y = src
     direction = math.sqrt((dst_x - x)**2 + (dst_y - y)**2)
