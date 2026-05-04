f5b1c50665ee881dc631af90fd743cd9f7e3cec0
Sabbe8 <sabel.calle@gmail.com>
2026-05-04 10:25:17 +0200

    Update camera.py

--- pi/camera.py
+++ pi/camera.py
@@ -3,26 +3,29 @@ import cv2
 
 app = Flask(__name__)
 
+# starta kamera
 camera = cv2.VideoCapture(0)
 
-def generate():
+def generate_frames():
     while True:
         success, frame = camera.read()
 
         if not success:
             break
 
-        ret, buffer = cv2.imencode('.jpg', frame)
-        frame = buffer.tobytes()
+        else:
+            _, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
 
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-
-@app.route('/video')
-def video():
-    return Response(generate(),
-        mimetype='multipart/x-mixed-replace; boundary=frame')
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
-app.run(host='0.0.0.0', port=8000)
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
