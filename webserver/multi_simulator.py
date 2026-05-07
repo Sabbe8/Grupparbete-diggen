c6803756c1f1a7dd7b9cc5a88908bf0cfd9d7004
Sabbe8 <sabel.calle@gmail.com>
2026-05-07 14:12:28 +0200

    Update multi_simulator.py

--- webserver/multi_simulator.py
+++ webserver/multi_simulator.py
@@ -5,9 +5,7 @@ import random
 
 r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
-# =========================
-# SKAPA DRÖNARE
-# =========================
+
 drones = {}
 
 NUM_DRONES = 5  # ändra till 2–10
@@ -23,24 +21,22 @@ for i in range(1, NUM_DRONES + 1):
     }
 
 
-# =========================
-# LOOP (SIMULERING)
-# =========================
+
 while True:
 
     for i, d in drones.items():
 
-        # 🟢 slumpmässig rörelse
+
         d["latitude"] += random.uniform(-d["speed"], d["speed"])
         d["longitude"] += random.uniform(-d["speed"], d["speed"])
 
-        # 🔴 ibland "busy"
+    
         if random.random() < 0.01:
             d["status"] = "busy"
         else:
             d["status"] = "idle"
 
-        # 🧠 skriv till Redis
+
         r.set(f"drone:{i}", json.dumps(d))
 
     time.sleep(1)
